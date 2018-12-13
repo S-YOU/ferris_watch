1e4d72f30fc230c8beee3242ecf97d37d4c14981
S-YOU <S-YOU@users.noreply.github.com>
2018-12-13 20:25:01 +0900

    cmd arguments

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 use log::debug;
-use clap::App;
+use clap::{App, Arg};
 
 fn main() -> Result<(), failure::Error> {
     env_logger::init();
     debug!("ferris_watch starting...");
 
-    let matches = App::new("ferris_watch") .version("0.1.0")
-        .author("Your Name <yourname@example.com>") .about("cute watch command") .get_matches();
+    let matches = App::new("ferris_watch").version("0.1.0")
+        .author("Your Name <yourname@example.com>").about("cute watch command")
+        .arg(
+            Arg::with_name("command")
+                .required(true)
+                .multiple(true)
+                .help("The command to run periodically"),
+        )
+        .get_matches();
 
     Ok(())
 }
